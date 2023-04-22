8af972b333f76e612d8428d8056b77fa9474045b
Luz Helber <helbersap@gmail.com>
2023-04-22 15:13:59 -0300

    endpoint findAll para usuario

--- src/main/java/br/com/projetos/api/services/UserService.java
+++ src/main/java/br/com/projetos/api/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.projetos.api.services;
 
 import br.com.projetos.api.domain.Usuario;
 
+import java.util.List;
+
 public interface UserService {
 
     Usuario findById(Integer id);
+    List<Usuario> findAll();
 }
