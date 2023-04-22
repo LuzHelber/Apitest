4d46e6bf0859b839b957d324bf5b00d9913d2506
Luz Helber <helbersap@gmail.com>
2023-04-22 11:54:32 -0300

    interface use repository

---
+++ src/main/java/br/com/projetos/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.projetos.api.repositories;
+
+import br.com.projetos.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
