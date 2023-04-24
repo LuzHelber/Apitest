e84665594218e5d8db7ffe167b4c7f738dadc42b
Luz Helber <helbersap@gmail.com>
2023-04-24 15:06:13 -0300

    criação de tes findbyid

---
+++ src/test/java/br/com/projetos/api/services/impl/UserServiceImplTest.java
@@ -0,0 +1,81 @@
+package br.com.projetos.api.services.impl;
+
+import br.com.projetos.api.domain.Usuario;
+import br.com.projetos.api.domain.dto.UserDTO;
+import br.com.projetos.api.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    public static final Integer ID       = 1;
+    public static final String  NAME     = "Helber";
+    public static final String  EMAIL    = "helber@mail.com";
+    public static final String  PASSWORD = "3663";
+    @InjectMocks
+    private UserServiceImpl service;
+    @Mock
+    private UserRepository repository;
+    @Mock
+    private ModelMapper mapper;
+
+    private Usuario usuario;
+    private UserDTO userDTO;
+    private Optional<Usuario> optionalUsuario;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+
+
+    }
+
+    @Test
+    void whenfindByIdThenReturnAnUserInstance() {
+        when(repository.findById(anyInt())).thenReturn(optionalUsuario);
+
+        Usuario response = service.findById(ID);
+
+        assertNotNull(response);
+        assertEquals(Usuario.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    private void startUser() {
+        usuario = new Usuario(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+        optionalUsuario = Optional.of(new Usuario(ID, NAME, EMAIL, PASSWORD));
+    }
+}
