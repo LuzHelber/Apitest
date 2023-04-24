08aad0cc9d09139ead40e9551ccbe6b0b96483e7
Luz Helber <helbersap@gmail.com>
2023-04-24 15:27:42 -0300

    teste para classe UserServiceImplTest "objeto nao encontrado"

--- src/test/java/br/com/projetos/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/projetos/api/services/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package br.com.projetos.api.services.impl;
 import br.com.projetos.api.domain.Usuario;
 import br.com.projetos.api.domain.dto.UserDTO;
 import br.com.projetos.api.repositories.UserRepository;
+import br.com.projetos.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -58,6 +59,18 @@ class UserServiceImplTest {
         assertEquals(NAME, response.getName());
         assertEquals(EMAIL, response.getEmail());
         assertEquals(PASSWORD, response.getPassword());
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnObjectNotFoundException() {
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException("Objeto não encontrado"));
+
+        try{
+            service.findById(ID);
+        } catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals("Objeto não encontrado", ex.getMessage());
+        }
 
     }
 
